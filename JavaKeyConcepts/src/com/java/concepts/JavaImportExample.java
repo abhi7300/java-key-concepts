024c9d19158b3a92d74e640f6b8ed4e5766d3251
Abhishek <abhishek1010mukherjee@gmail.com>
2018-05-06 21:43:02 +0530

    new commit

---
+++ JavaKeyConcepts/src/com/java/concepts/JavaImportExample.java
@@ -0,0 +1,12 @@
+package com.java.concepts;
+
+public class JavaImportExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Did you know that java.lang package classes are imported automatically?");
+		System.out.println("Second line of statement.");
+		System.out.println("Third line of statement.");
+	}
+
+}
