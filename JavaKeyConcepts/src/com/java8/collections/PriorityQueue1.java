330b93f0dc9b40a4d4adfd196f13478c00291609
Abhishek <abhishek1010mukherjee@gmail.com>
2018-08-15 00:26:32 +0530

    new concepts

--- JavaKeyConcepts/src/com/java8/collections/PriorityQueue1.java
+++ JavaKeyConcepts/src/com/java8/collections/PriorityQueue1.java
@@ -1,5 +1,8 @@
 package com.java8.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueue1 {
@@ -7,22 +10,40 @@ public class PriorityQueue1 {
 	public static void main(String[] args) {
 		
 		
-		PriorityQueue pq = new PriorityQueue<>();
+		PriorityQueue<String> pqString = new PriorityQueue<>();
 		
-		pq.add("first");
-		pq.add("second");
-		pq.add("third");
-		pq.offer("fourth");
 		
-		System.out.println(pq.size()+" ");
-		System.out.println(pq.peek()+" ");
-		System.out.println(pq.size()+" ");
-		System.out.println(pq.peek()+" ");
-		System.out.println(pq.size()+" ");
-		System.out.println(pq.poll()+" ");
-		System.out.println(pq.size()+" ");
-		System.out.println(pq.poll()+" ");
-		System.out.println(pq.size()+" ");
+		pqString.add("first");
+		pqString.add("second");
+		pqString.add("third");
+		pqString.offer("fourth");
+		
+		/*System.out.println(pqString.size()+" ");
+		System.out.println(pqString.peek()+" ");
+		System.out.println(pqString.size()+" ");
+		System.out.println(pqString.peek()+" ");
+		System.out.println(pqString.size()+" ");
+		System.out.println(pqString.poll()+" ");
+		System.out.println(pqString.size()+" ");
+		System.out.println(pqString.poll()+" ");
+		System.out.println(pqString.size()+" ");*/
+		
+		
+		List<Integer> intList = new ArrayList<Integer>();
+		
+		intList.add(5);
+		intList.add(4);
+		intList.add(6);
+		intList.add(40);
+		intList.add(8);
+		intList.add(1);
+		intList.add(7);
+		intList.add(2);
+		intList.add(10);
+		intList.add(4);
+		Collections.sort(intList);
+		
+		System.out.println(intList.toString());
 		
 	}
 }
--- JavaKeyConcepts/src/com/java8/datetime/BasicsOfNewDateTimeAPI.java
+++ JavaKeyConcepts/src/com/java8/datetime/BasicsOfNewDateTimeAPI.java
@@ -11,13 +11,68 @@ public class BasicsOfNewDateTimeAPI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("~~~~~~~~~~~~~~~~Creating using now()~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
 		System.out.println("Date:"+LocalDate.now());
 		System.out.println("Time:"+LocalTime.now());
 		System.out.println("DateTime:"+LocalDateTime.now());
+		System.out.println();
 		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+		System.out.println("~~~~~~~~~~~~~~~~Creating using of()~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+		System.out.println("Date:"+LocalDate.of(2018, 8, 13));
+		System.out.println("Time:"+LocalTime.of(13, 0, 0, 0));
+		System.out.println("DateTime:"+LocalDateTime.of(2018, 8, 13, 13, 0));
+		System.out.println();
 		
-		System.out.println("DateTime formatted:"+LocalDateTime.now().format(dtf));
+		System.out.println("~~~~~~~~~~~~~~~~Formatting~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+		System.out.println("Rule: Always look at the DateTimeFormatter type. "
+				+ "\n If what it expects is not provided to it, it will throw an exception."
+				+ "\n 1. ISO_DATE must be provided with date."
+				+ "\n 2. ISO_TIME must be provided with time."
+				+ "\n 3. ISO_DATE_TIME must be provided with both date and time."
+				+ "\n 4. If anything (date/time) is provided, which dtf doesn't need, it will be discarded.");
+		System.out.println();
+		
+		// can use a custom format
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+		
+		// can use a predefined format
+		DateTimeFormatter dtf2 = DateTimeFormatter.ISO_DATE;
+		DateTimeFormatter dtf3 = DateTimeFormatter.ISO_TIME;
+		DateTimeFormatter dtf4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter dtf5 = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+		
+		System.out.println("DateTime formatted 1:"+LocalDate.now().format(DateTimeFormatter.ISO_DATE));
+		System.out.println("DateTime formatted 1:"+LocalTime.now().format(DateTimeFormatter.ISO_TIME));
+		System.out.println("DateTime formatted 1:"+LocalDateTime.now().format(DateTimeFormatter.ISO_TIME));
+		
+		System.out.println("DateTime formatted 2:"+dtf2.format(LocalDate.now()));
+		//System.out.println("DateTime formatted 2:"+dtf2.format(LocalTime.now()));
+		System.out.println("DateTime formatted 2:"+dtf2.format(LocalDateTime.now()));
+		
+		//System.out.println("DateTime formatted 3:"+dtf3.format(LocalDate.now()));
+		System.out.println("DateTime formatted 3:"+dtf3.format(LocalTime.now()));
+		System.out.println("DateTime formatted 3:"+dtf3.format(LocalDateTime.now()));
+		
+		//System.out.println("DateTime formatted 4:"+dtf4.format(LocalDate.now()));
+		//System.out.println("DateTime formatted 4:"+dtf4.format(LocalTime.now()));
+		System.out.println("DateTime formatted 4:"+dtf4.format(LocalDateTime.now()));
+		
+		
+		System.out.println();
+		
+		
+		System.out.println("~~~~~~~~~~~~~~~~Parsing~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+		System.out.println("The following methods do not throw exception:");
+		System.out.println("Parsed Date:"+LocalDate.parse("2018-08-13"));//Uses DateTimeFormatter.ISO_LOCAL_DATE by default
+		System.out.println("Parsed Time:"+LocalTime.parse("13:00:00"));//Uses DateTimeFormatter.ISO_LOCAL_TIME by default
+		System.out.println("Parsed DateTime 1:"+LocalDateTime.parse("2018-08-13T13:00:00"));//Uses DateTimeFormatter.ISO_LOCAL_DATE_TIME by default
+		System.out.println("Parsed DateTime 2:"+LocalDateTime.parse("2018-08-13T13:00:00",DateTimeFormatter.ISO_DATE_TIME));
+		
+		System.out.println();
 	}
 
 }
